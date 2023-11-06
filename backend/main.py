6f42dbe06966e761bc4651d75e03192e83ad7c19
At1X <amirmahdijafarian@gmail.com>
2023-11-06 19:20:37 +0330

    add simple backend to interact with

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, make_response
+from flask_cors import CORS, cross_origin
+
+USERNAME = "admin"
+PASSWORD = "admin"
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+@app.route('/login/', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def login():
+    data = request.get_json()
+    if (data['username'] == USERNAME and data['password'] == PASSWORD):
+        return make_response("Login Success", 200)
+    else:
+        return make_response("Login Failed", 401)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import InstagramTextLogo from "./assets/instagram-logo/Instagram_logo..png";
 import GooglePlay from "./assets/stores-logo/GooglePlay.png";
 import Microsoft from "./assets/stores-logo/Microsoft.png";
 
+import { BACKEND_API } from "../CONST_VARS";
+
+import { useRef, useState } from "react";
+import axios from "axios";
+
 function App() {
+  const username = useRef();
+  const password = useRef();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [hasErr, setHasErr] = useState(false);
   const footer_text = [
     "Meta",
     "About",
@@ -19,71 +28,101 @@ function App() {
     "Contact Uploading & Non-Users",
     "Meta Verified",
   ];
+
+  const loginSend = () => {
+    axios
+      .post(`${BACKEND_API}/login/`, {
+        username: username.current.value,
+        password: password.current.value,
+      })
+      .then((res) => {
+        if (res.status === 200) setIsLoggedIn(true);
+      })
+      .catch(() => {
+        setHasErr(true);
+      });
+  };
+
   return (
     <>
       <div className="h-screen w-screen flex flex-col justify-center items-center">
-        <div className="h-fit w-screen flex flex-row justify-center items-center mt-5">
-          <div className="h-full w-full md:flex flex-row justify-end items-center hidden">
-            <img className="scale-75" src={ScreenShot} />
-          </div>
-          <div className="h-full w-full flex flex-row justify-center mb-10 md:mb-0 md:justify-start items-center">
-            <div className="flex flex-col gap-2">
-              <div className="w-[350px] h-[324px] border-[1px] border-[219, 219, 219] rounded-sm flex flex-col items-center justify-center">
-                <img className="scale-[0.65]" src={InstagramTextLogo} />
-                <input
-                  className="border-[1px] border-[219, 219, 219] outline-none rounded-[3px] w-4/5 py-[6px] px-2 placeholder:text-[12px] placeholder:text[75, 75, 75]"
-                  placeholder="Phone number, username, or email"
-                />
-                <input
-                  className="border-[1px] border-[219, 219, 219] outline-none rounded-[3px] w-4/5 py-[6px] px-2 placeholder:text-[12px] placeholder:text[75, 75, 75] mt-1"
-                  placeholder="Password"
-                />
-                <button className="w-4/5 rounded-lg text-slate-50 bg-[#0095F6] font-bold text-center py-[6px] mt-3">
-                  Log in
-                </button>
-                <span className="mt-4 text-sm cursor-pointer text-[#003783]">
-                  Forgot Password?
-                </span>
+        {!isLoggedIn ? (
+          <>
+            <div className="h-fit w-screen flex flex-row justify-center items-center mt-5">
+              <div className="h-full w-full md:flex flex-row justify-end items-center hidden">
+                <img className="scale-75" src={ScreenShot} />
               </div>
-              <div className="w-[350px] h-[63px] border-[1px] border-[219, 219, 219] rounded-sm flex flex-row justify-center items-center gap-2 text-[14px]">
-                <span>{`Don't have an account?`}</span>
-                <span className="text-[#0095F6] font-bold cursor-pointer">
-                  Sign up
-                </span>
-              </div>
-              <div className="w-[350px] flex flex-row justify-center items-center">
-                Get the app.
-              </div>
-              <div className="w-[350px] flex flex-row justify-center items-center gap-1">
-                <img className="w-[120px] cursor-pointer" src={GooglePlay} />
-                <img className="w-[120px] cursor-pointer" src={Microsoft} />
+              <div className="h-full w-full flex flex-row justify-center mb-10 md:mb-0 md:justify-start items-center">
+                <div className="flex flex-col gap-2">
+                  <div className="w-[350px] h-[324px] border-[1px] border-[219, 219, 219] rounded-sm flex flex-col items-center justify-center">
+                    <img className="scale-[0.65]" src={InstagramTextLogo} />
+                    <input
+                      className="border-[1px] border-[219, 219, 219] outline-none rounded-[3px] w-4/5 py-[6px] px-2 placeholder:text-[12px] placeholder:text[75, 75, 75]"
+                      placeholder="Phone number, username, or email"
+                      ref={username}
+                    />
+                    <input
+                      className="border-[1px] border-[219, 219, 219] outline-none rounded-[3px] w-4/5 py-[6px] px-2 placeholder:text-[12px] placeholder:text[75, 75, 75] mt-1"
+                      placeholder="Password"
+                      ref={password}
+                    />
+                    <button
+                      onClick={loginSend}
+                      className={`w-4/5 rounded-lg text-slate-50 ${
+                        hasErr ? "bg-[#692224]" : "bg-[#0095F6]"
+                      } font-bold text-center py-[6px] mt-3`}
+                    >
+                      {hasErr ? "Log In Failed." : "Log In"}
+                    </button>
+                    <span className="mt-4 text-sm cursor-pointer text-[#003783]">
+                      Forgot Password?
+                    </span>
+                  </div>
+                  <div className="w-[350px] h-[63px] border-[1px] border-[219, 219, 219] rounded-sm flex flex-row justify-center items-center gap-2 text-[14px]">
+                    <span>{`Don't have an account?`}</span>
+                    <span className="text-[#0095F6] font-bold cursor-pointer">
+                      Sign up
+                    </span>
+                  </div>
+                  <div className="w-[350px] flex flex-row justify-center items-center">
+                    Get the app.
+                  </div>
+                  <div className="w-[350px] flex flex-row justify-center items-center gap-1">
+                    <img
+                      className="w-[120px] cursor-pointer"
+                      src={GooglePlay}
+                    />
+                    <img className="w-[120px] cursor-pointer" src={Microsoft} />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        {/* FOOTER  */}
-        <div className="flex flex-col gap-3 h-fit mb-4 items-center justify-center">
-          <div className="flex flex-row w-3/5 md:w-full flex-wrap justify-center items-center gap-3">
-            {footer_text.map((element) => {
-              return (
-                <span
-                  key={element}
-                  className="text-sm text-[#737373] cursor-pointer"
-                >
-                  {element}
+            <div className="flex flex-col gap-3 h-fit mb-4 items-center justify-center">
+              <div className="flex flex-row w-3/5 md:w-full flex-wrap justify-center items-center gap-3">
+                {footer_text.map((element) => {
+                  return (
+                    <span
+                      key={element}
+                      className="text-sm text-[#737373] cursor-pointer"
+                    >
+                      {element}
+                    </span>
+                  );
+                })}
+              </div>
+              <div className="flex flex-row w-full justify-center items-center gap-3 mb-5">
+                <span className="text-sm text-[#737373] cursor-pointer">
+                  English
                 </span>
-              );
-            })}
-          </div>
-          <div className="flex flex-row w-full justify-center items-center gap-3 mb-5">
-            <span className="text-sm text-[#737373] cursor-pointer">
-              English
-            </span>
-            <span className="text-sm text-[#737373] cursor-pointer">
-              © 2023 Atid & Ali D from IUT
-            </span>
-          </div>
-        </div>
+                <span className="text-sm text-[#737373] cursor-pointer">
+                  © 2023 Atid & Ali D from IUT
+                </span>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div>Successfully Logged In.</div>
+        )}
       </div>
     </>
   );
